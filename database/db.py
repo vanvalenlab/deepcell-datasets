52434505fd061b7e3bac0879e6fe71b79f867a94
William Graf <7930703+willgraf@users.noreply.github.com>
2020-05-21 15:23:40 -0700

    whitespace and future imports (for consistency with rest of files)

--- database/db.py
+++ database/db.py
@@ -1,6 +1,12 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
 from flask_mongoengine import MongoEngine
 
+
 db = MongoEngine()
+
 
 def initialize_db(app):
     db.init_app(app)
