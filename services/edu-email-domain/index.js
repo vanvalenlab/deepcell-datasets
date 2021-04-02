d52a9178664f5113ebae784905134fe72c06a6ba
William Graf <7930703+willgraf@users.noreply.github.com>
2021-04-02 12:35:46 -0700

    Update presignup hook to check all domains in DOMAIN_WHITELIST

--- services/edu-email-domain/index.js
+++ services/edu-email-domain/index.js
@@ -1,8 +1,27 @@
+function testEmail(email) {
+  // check that the input string is an well formed email
+  email = email.toString().toLowerCase();
+  var emailFilter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
+  if (!emailFilter.test(email)) {
+    return false;
+  }
+
+  // A whitelist of domains to enable registration
+  var domainWhitelist = (process.env.DOMAIN_WHITELIST || '.edu').split(',');
+
+  for (let s of domainWhitelist) {
+    if (email.endsWith(s)) {
+      return true;
+    }
+  }
+  // email is well formed but not in the whitelist
+  return false;
+};
+
 exports.handler = (event, context, callback) => {
-    var eduRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+edu))$/i;
     var error = null;
-    if (!eduRegEx.test(event.userName)) {
-        error = new Error('Only .edu emails are supported.');
+    if (!testEmail(event.userName)) {
+      error = new Error('Only university/academic email domains are supported.');
     }
     callback(error, event);
 };
