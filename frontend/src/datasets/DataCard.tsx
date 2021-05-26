2280a29a9bee981839495299ba3e47126b046b28
William Graf <7930703+willgraf@users.noreply.github.com>
2021-05-26 13:22:09 -0700

    Update data card props to include new schema values and improve spacing.

--- frontend/src/datasets/DataCard.tsx
+++ frontend/src/datasets/DataCard.tsx
@@ -3,6 +3,8 @@ import { Storage } from '@aws-amplify/storage';
 import Alert from 'react-bootstrap/Alert';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
 
 const openInNewTab = (url: string) => {
   const newWindow = window.open(url, '_blank', 'noopener,noreferrer')
@@ -12,7 +14,9 @@ const openInNewTab = (url: string) => {
 export default function DataCard (props: any) {
   const [error, setError] = useState<string|null>(null);
   const title = props.title || 'Data Title';
-  const text = props.text || 'Data description';
+  const description = props.description || 'Data description';
+  const samples = props.samples || 'Types of images';
+  const imagingPlatform = props.imagingPlatform || 'Imaging Platform';
   const thumbnail = props.thumbnail || '';
   const objectKey = props.objectKey || '';
 
@@ -34,8 +38,18 @@ export default function DataCard (props: any) {
     <Card>
       <Card.Img variant="top" src={thumbnail} />
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
-        <Card.Text>{text}</Card.Text>
+        <Card.Title className="mb-0">{title}</Card.Title>
+        <ListGroup variant="flush">
+          <ListGroupItem className="px-0 my-0">
+            <Card.Text><strong>Imaging Platform:</strong> {imagingPlatform}</Card.Text>
+          </ListGroupItem>
+          <ListGroupItem className="px-0 my-0">
+            <Card.Text><strong>Samples:</strong> {samples}</Card.Text>
+          </ListGroupItem>
+          <ListGroupItem className="px-0 my-0">
+            <Card.Text><strong>Description:</strong> {description}</Card.Text>
+          </ListGroupItem>
+        </ListGroup>
         <Button variant="dark" onClick={onClick}>Download</Button>
         { (error !== null) ?
           <Alert variant="danger" dismissible onClose={() => setError(null)}>
