d974afd3521f8b611e16f4f945c56c047cc9d5b8
willgraf <7930703+willgraf@users.noreply.github.com>
2021-11-16 15:09:15 -0800

    Add the Tracked Nuclei dataset to the curated datasets page. (#78)
    * Add thumbnail for new tracking data.
    
    * Add new Dataset object for tracking data.
    
    * Change name to LiveCellNet v0.1.

--- frontend/src/datasets/AllDatasets.tsx
+++ frontend/src/datasets/AllDatasets.tsx
@@ -7,13 +7,21 @@
 
 const AllDatasets = [
   {
-    title: 'TissueNet Version 1.0',
+    title: 'TissueNet Version v1.0',
     objectKey: 'tissuenet/tissuenet_v1.0.zip',
     thumbnail: '/images/multiplex_overlay.webp',
     imagingPlatform: 'Multiplexed imaging',
     samples: '2D tissue',
     description: 'This is the first release of the TissueNet dataset from Greenwald, Miller et al.',
-  }
+  },
+  {
+    title: 'LiveCellNet v0.1',
+    objectKey: 'tracking-nuclear/val.trks',
+    thumbnail: '/images/3t3_nuclear_outlines.webp',
+    imagingPlatform: 'Fluorescent Microscopy',
+    samples: '2D culture',
+    description: 'This is a subset of the Nuclear Tracking dataset.',
+  },
 ];
 
 export default AllDatasets;
